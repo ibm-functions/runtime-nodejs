9d90cb12d3bbd6f4e9c37e8a0d3fe62ec0e89eb6
Carlos Santana <csantana23@gmail.com>
2017-09-13 11:05:18 -0400

    Travis deploy runtime and openwhisk (#1)
    * use -e to override
    
    * point to ibm-functions dockerhub
    
    * add initial test
    
    * quick test action
    
    * add -i wsk
    
    * debug action not running
    
    * now it worked

--- tools/travis/build.sh
+++ tools/travis/build.sh
@@ -15,8 +15,23 @@ docker tag action-nodejs-ibm-v8.5 testing/action-nodejs-ibm-v8.5
 
 
 # Build OpenWhisk
+
 cd $WHISKDIR
 export OPENWHISK_HOME=$WHISKDIR
+IMAGE_PREFIX="testing"
+#superfast option
+docker pull openwhisk/controller
+docker tag openwhisk/controller ${IMAGE_PREFIX}/controller
+docker pull openwhisk/invoker
+docker tag openwhisk/invoker ${IMAGE_PREFIX}/invoker
+docker pull openwhisk/nodejs6action
+docker tag openwhisk/nodejs6action ${IMAGE_PREFIX}/nodejs6action
+
+#Build CLI
+TERM=dumb ./gradlew \
+:tools:cli:distDocker \
+-PdockerImagePrefix=${IMAGE_PREFIX}
+
 #fast options only build what we need
 #TERM=dumb ./gradlew \
 #:core:controller:distDocker \
@@ -29,16 +44,29 @@ export OPENWHISK_HOME=$WHISKDIR
 #TERM=dumb ./gradlew distDocker -PdockerImagePrefix=testing
 
 # Deploy OpenWhisk
-#cd $WHISKDIR/ansible
-#overwrite default one for now
-#cp $ROOTDIR/tools/travis/ansible/group_vars/all group_vars/all
-#ANSIBLE_CMD="ansible-playbook -i environments/local -e docker_image_prefix=testing"
-#$ANSIBLE_CMD setup.yml
-#$ANSIBLE_CMD prereq.yml
-#$ANSIBLE_CMD couchdb.yml
-#$ANSIBLE_CMD initdb.yml
-#$ANSIBLE_CMD wipe.yml
-#$ANSIBLE_CMD openwhisk.yml
+cd $WHISKDIR/ansible
+RUNTIMES_MANIFEST='{"runtimesManifest":{"defaultImagePrefix": "openwhisk", "defaultImageTag": "latest", "blackboxes": [{"name": "dockerskeleton"}], "runtimes": { "nodejs": [{"deprecated": false, "kind": "nodejs-ibm:8.5", "image": {"name": "action-nodejs-ibm-v8.5"}}, {"default": true, "deprecated": false, "kind": "nodejs:6", "image": {"name": "nodejs6action"}}]}}}'
+ANSIBLE_CMD="ansible-playbook -i environments/local -e docker_image_prefix=testing"
+$ANSIBLE_CMD setup.yml
+$ANSIBLE_CMD prereq.yml
+$ANSIBLE_CMD couchdb.yml
+$ANSIBLE_CMD initdb.yml
+$ANSIBLE_CMD wipe.yml
+$ANSIBLE_CMD openwhisk.yml -e "${RUNTIMES_MANIFEST}"
 
+docker images
+docker ps
+
+cat $WHISKDIR/whisk.properties
+curl -s -k https://172.17.0.1 | jq .
+curl -s -k https://172.17.0.1/api/v1 | jq .
+
+#Deployment
+WHISK_APIHOST="172.17.0.1"
+WHISK_AUTH=`cat ${WHISKDIR}/ansible/files/auth.guest`
+WHISK_CLI="${WHISKDIR}/bin/wsk -i"
+
+${WHISK_CLI} property set --apihost ${WHISK_APIHOST} --auth ${WHISK_AUTH} 
+${WHISK_CLI} property get
 
 
---
+++ tools/travis/test.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -ex
+
+# Build script for Travis-CI.
+
+SCRIPTDIR=$(cd $(dirname "$0") && pwd)
+ROOTDIR="$SCRIPTDIR/../.."
+HOMEDIR="$SCRIPTDIR/../../../"
+WHISKDIR="$ROOTDIR/../openwhisk"
+
+#Deployment
+WHISK_CLI="${WHISKDIR}/bin/wsk -i"
+
+# Check kind from manifest nodejs-ibm:8.5
+curl -s -k https://${WHISK_APIHOST} | jq '.runtimes.nodejs | any(.kind == "nodejs-ibm:8.5")'
+
+# Run a simple action using the kind
+${WHISK_CLI} action update getNodeVersion ${ROOTDIR}/tests/dat/getNodeVersion.js --kind nodejs-ibm:8.5
+${WHISK_CLI} action get getNodeVersion
+${WHISK_CLI} action invoke getNodeVersion -b
+
+#For some reason there no activations, maybe index not ready
+#${WHISK_CLI} activation get --last
+
+#DEBUG logs
+#cat /tmp/wsklogs/controller0/controller0_logs.log
+#cat /tmp/wsklogs/controller1/controller1_logs.log
+#cat /tmp/wsklogs/invoker0/invoker0_logs.log
+#cat /tmp/wsklogs/invoker1/invoker1_logs.log
