f9b33a7be402ef7bd1e110709bb7f196a9b6bf59
Carlos Santana <csantana@us.ibm.com>
2017-09-13 09:00:26 -0400

    use -e to override

--- tools/travis/build.sh
+++ tools/travis/build.sh
@@ -18,27 +18,30 @@ docker tag action-nodejs-ibm-v8.5 testing/action-nodejs-ibm-v8.5
 cd $WHISKDIR
 export OPENWHISK_HOME=$WHISKDIR
 #fast options only build what we need
-#TERM=dumb ./gradlew \
-#:core:controller:distDocker \
-#:core:invoker:distDocker \
-#:core:nodejs6Action:distDocker \
-#:tools:cli:distDocker \
-#-PdockerImagePrefix=testing
+TERM=dumb ./gradlew \
+:core:controller:distDocker \
+:core:invoker:distDocker \
+:core:nodejs6Action:distDocker \
+:tools:cli:distDocker \
+-PdockerImagePrefix=testing
 
 #long version
 #TERM=dumb ./gradlew distDocker -PdockerImagePrefix=testing
 
 # Deploy OpenWhisk
-#cd $WHISKDIR/ansible
-#overwrite default one for now
-#cp $ROOTDIR/tools/travis/ansible/group_vars/all group_vars/all
-#ANSIBLE_CMD="ansible-playbook -i environments/local -e docker_image_prefix=testing"
-#$ANSIBLE_CMD setup.yml
-#$ANSIBLE_CMD prereq.yml
-#$ANSIBLE_CMD couchdb.yml
-#$ANSIBLE_CMD initdb.yml
-#$ANSIBLE_CMD wipe.yml
-#$ANSIBLE_CMD openwhisk.yml
+cd $WHISKDIR/ansible
+RUNTIMES_MANIFEST='{"runtimesManifest":{"defaultImagePrefix": "openwhisk", "defaultImageTag": "latest", "blackboxes": [{"name": "dockerskeleton"}], "runtimes": { "nodejs": [{"deprecated": false, "kind": "nodejs-ibm:8.5", "image": {"name": "action-nodejs-ibm-v8.5"}}, {"default": true, "deprecated": false, "kind": "nodejs:6", "image": {"name": "nodejs6action"}}]}}}'
+ANSIBLE_CMD="ansible-playbook -i environments/local -e docker_image_prefix=testing"
+$ANSIBLE_CMD setup.yml
+$ANSIBLE_CMD prereq.yml
+$ANSIBLE_CMD couchdb.yml
+$ANSIBLE_CMD initdb.yml
+$ANSIBLE_CMD wipe.yml
+$ANSIBLE_CMD openwhisk.yml -e "${RUNTIMES_MANIFEST}"
+
+curl -s -k https://172.17.0.1
+
+jq --help
 
 
 
