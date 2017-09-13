5911255a4bf3add97c2d4e33b9f8c9f68a8c079e
Carlos Santana <csantana@us.ibm.com>
2017-09-13 10:54:54 -0400

    now it worked

--- tools/travis/test.sh
+++ tools/travis/test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -x
+set -ex
 
 # Build script for Travis-CI.
 
@@ -18,10 +18,12 @@ curl -s -k https://${WHISK_APIHOST} | jq '.runtimes.nodejs | any(.kind == "nodej
 ${WHISK_CLI} action update getNodeVersion ${ROOTDIR}/tests/dat/getNodeVersion.js --kind nodejs-ibm:8.5
 ${WHISK_CLI} action get getNodeVersion
 ${WHISK_CLI} action invoke getNodeVersion -b
-${WHISK_CLI} activation get --last
+
+#For some reason there no activations, maybe index not ready
+#${WHISK_CLI} activation get --last
 
 #DEBUG logs
-cat /tmp/wsklogs/controller0/controller0_logs.log
-cat /tmp/wsklogs/controller1/controller1_logs.log
-cat /tmp/wsklogs/invoker0/invoker0_logs.log
-cat /tmp/wsklogs/invoker1/invoker1_logs.log
+#cat /tmp/wsklogs/controller0/controller0_logs.log
+#cat /tmp/wsklogs/controller1/controller1_logs.log
+#cat /tmp/wsklogs/invoker0/invoker0_logs.log
+#cat /tmp/wsklogs/invoker1/invoker1_logs.log
