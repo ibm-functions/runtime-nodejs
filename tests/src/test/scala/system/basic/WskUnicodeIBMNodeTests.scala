48d4804701ae809359a41d54d09cb707de6b20eb
Carlos Santana <csantana@us.ibm.com>
2017-09-16 16:10:12 -0400

    Add Unicode tests

---
+++ tests/src/test/scala/system/basic/WskUnicodeIBMNodeTests.scala
@@ -0,0 +1,15 @@
+package system.basic
+
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+
+import common.JsHelpers
+import common.WskTestHelpers
+
+@RunWith(classOf[JUnitRunner])
+class WskUnicodeIBMNodeTests extends WskUnicodeTests with WskTestHelpers with JsHelpers {
+
+  override lazy val actionKind = "nodejs-ibm:8.5"
+  override lazy val actionSource = "unicode.js"
+
+}
