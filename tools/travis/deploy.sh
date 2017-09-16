29aed26fd768b26cdc8bc8f437ecc5c0eb2bc70f
Carlos Santana <csantana@us.ibm.com>
2017-09-16 14:12:41 -0400

    build image via gradlew and distDocker

--- tools/travis/deploy.sh
+++ tools/travis/deploy.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 set -eu
 
+# Build script for Travis-CI.
+
+SCRIPTDIR=$(cd $(dirname "$0") && pwd)
+ROOTDIR="$SCRIPTDIR/../.."
+
 dockerhub_image_prefix="$1"
-dockerhub_image_name="$2"
-dockerhub_image_tag="$3"
-docker_build_path="$4"
-dockerhub_image="${dockerhub_image_prefix}/${dockerhub_image_name}:${dockerhub_image_tag}"
+dockerhub_image_tag="$2"
 
 docker login -u "${DOCKER_USER}" -p "${DOCKER_PASSWORD}"
 
-echo docker build $docker_build_path --tag ${dockerhub_image}
-docker build $docker_build_path --tag ${dockerhub_image}
-
-echo docker push ${dockerhub_image}
-docker push ${dockerhub_image}
+cd ${ROOTDIR}
+TERM=dumb ./gradlew :8.5:distDocker \
+-PdockerImagePrefix=${dockerhub_image_prefix} 
+-PdockerImageTag=${dockerhub_image_tag}
+-PdockerRegistry=docker.io
