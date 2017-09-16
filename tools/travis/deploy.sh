292efca6320940819a663f6e1821017a1132d491
Carlos Santana <csantana@us.ibm.com>
2017-09-16 19:11:24 -0400

    enable deploy to registry

--- tools/travis/deploy.sh
+++ tools/travis/deploy.sh
@@ -10,17 +10,16 @@ WHISKDIR="$ROOTDIR/../openwhisk"
 dockerhub_image_prefix="$1"
 dockerhub_image_tag="$2"
 
-#docker login -u "${DOCKER_USER}" -p "${DOCKER_PASSWORD}"
+docker login -u "${DOCKER_USER}" -p "${DOCKER_PASSWORD}"
 
 export OPENWHISK_HOME=$WHISKDIR
 cd $ROOTDIR
 pwd 
 ls
 DEBUG_CMD="TERM=dumb ./gradlew :8.5:distDocker -PdockerImagePrefix=${dockerhub_image_prefix} -PdockerImageTag=${dockerhub_image_tag}"
+echo $DEBUG_CMD
 TERM=dumb ./gradlew \
 :8.5:distDocker \
 -PdockerImagePrefix=${dockerhub_image_prefix} \
--PdockerImageTag=${dockerhub_image_tag}
-
-
-#-PdockerRegistry=docker.io
+-PdockerImageTag=${dockerhub_image_tag} \
+-PdockerRegistry=docker.io
