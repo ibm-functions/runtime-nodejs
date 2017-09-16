cd5bc03a5227f90dc045e18cf3eae76691f10eb2
Carlos Santana <csantana@us.ibm.com>
2017-09-16 16:15:14 -0400

    add OPENWHISK_HOME var for deploy.sh

--- tools/travis/deploy.sh
+++ tools/travis/deploy.sh
@@ -5,6 +5,7 @@ set -eu
 
 SCRIPTDIR=$(cd $(dirname "$0") && pwd)
 ROOTDIR="$SCRIPTDIR/../.."
+WHISKDIR="$ROOTDIR/../openwhisk"
 
 dockerhub_image_prefix="$1"
 dockerhub_image_tag="$2"
@@ -12,6 +13,7 @@ dockerhub_image_tag="$2"
 docker login -u "${DOCKER_USER}" -p "${DOCKER_PASSWORD}"
 
 cd ${ROOTDIR}
+export OPENWHISK_HOME=$WHISKDIR
 TERM=dumb ./gradlew :8.5:distDocker \
 -PdockerImagePrefix=${dockerhub_image_prefix} 
 -PdockerImageTag=${dockerhub_image_tag}
