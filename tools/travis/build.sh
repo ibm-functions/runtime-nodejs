1c1916d8a5625bc88845948cf8f0e8d2e26159f5
Carlos Santana <csantana@us.ibm.com>
2017-10-13 08:59:33 -0400

    add creds json

--- tools/travis/build.sh
+++ tools/travis/build.sh
@@ -56,7 +56,13 @@ $ANSIBLE_CMD openwhisk.yml
 docker images
 docker ps
 
+#update whisk.properties to add tests/credentials.json file to vcap.services.file, which is needed in tests
+VCAP_SERVICES_FILE="$(readlink -f $WHISKDIR/../tests/credentials.json)"
+WHISKPROPS_FILE="$WHISKDIR/whisk.properties"
+sed -i 's:^[ \t]*vcap.services.file[ \t]*=\([ \t]*.*\)$:vcap.services.file='$VCAP_SERVICES_FILE':'  $WHISKPROPS_FILE
+
 cat $WHISKDIR/whisk.properties
+
 curl -s -k https://172.17.0.1 | jq .
 curl -s -k https://172.17.0.1/api/v1 | jq .
 
