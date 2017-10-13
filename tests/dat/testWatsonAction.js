d2e96593c2f040522de9dee78dcb30e1e77ed1fd
Angela <aml2732@utexas.edu>
2017-10-13 08:01:36 -0500

    watson test (#32)

---
+++ tests/dat/testWatsonAction.js
@@ -0,0 +1,13 @@
+
+var DiscoveryV1 = require('watson-developer-cloud/discovery/v1');
+
+function main(args){
+  var discovery = new DiscoveryV1({
+    use_unauthenticated: true,
+    version_date: DiscoveryV1.VERSION_DATE_2017_08_01
+  });
+
+  var valueOfDiscoveryObjectVersion = discovery._options.qs.version;
+
+  return {message: valueOfDiscoveryObjectVersion}
+}
---
+++ tests/src/test/scala/actionContainers/IBMNodeJsActionWatsonTests.scala
@@ -0,0 +1,50 @@
+package actionContainers
+
+import common.TestHelpers
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+import common.WskTestHelpers
+import common.Wsk
+import common.WskProps
+import java.io.File
+import spray.json._
+import spray.json.DefaultJsonProtocol._
+import org.scalatest.BeforeAndAfterAll
+
+// Run test suite with : `./gradlew tests:test --tests *IBMNodeJsActionWatsonTests`
+
+@RunWith(classOf[JUnitRunner])
+class IBMNodeJsActionWatsonTests extends TestHelpers with WskTestHelpers with BeforeAndAfterAll {
+
+  implicit val wskprops: WskProps = WskProps()
+  var defaultKind = Some("nodejs-ibm:8")
+  val wsk = new Wsk
+  val datdir = System.getProperty("user.dir") + "/dat/"
+
+  it should "Test whether or not watson package is useable within a nodejs8 action" in withAssetCleaner(wskprops) {
+    (wp, assetHelper) =>
+      /*
+     Disclaimer : Does not Use / Connect to a watson service! Tests that the
+     watson-developer-cloud npm package is useable by verifying creating a new
+     discover object creates the expected object with the expected properties.
+       */
+
+      val file = Some(new File(datdir, "testWatsonAction.js").toString())
+
+      assetHelper.withCleaner(wsk.action, "testWatsonAction") { (action, _) =>
+        action.create(
+          "testWatsonAction",
+          file,
+          main = Some("main"),
+          kind = defaultKind,
+          parameters = Map("hostname" -> wskprops.apihost.toJson))
+      }
+
+      withActivation(wsk.activation, wsk.action.invoke("testWatsonAction")) { activation =>
+        val response = activation.response
+        response.result.get.fields.get("error") shouldBe empty
+        response.result.get.fields.get("message") should be(Some(JsString("2017-08-01")))
+      }
+
+  }
+}
