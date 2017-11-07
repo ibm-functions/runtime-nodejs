2e8b405e4b0e22368b96a2bacf6f277146462438
Carlos Santana <csantana23@gmail.com>
2017-11-07 00:24:04 -0500

    refactor gradle test tasks (#63)
    * refactire gradle test tasks
    
    * fix test reference using wskRest now

--- tests/src/test/scala/integration/CredentialsIBMNodeJsActionWatsonTests.scala
+++ tests/src/test/scala/integration/CredentialsIBMNodeJsActionWatsonTests.scala
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package actionContainers
+package integration
 
 import common.TestHelpers
 import org.junit.runner.RunWith
