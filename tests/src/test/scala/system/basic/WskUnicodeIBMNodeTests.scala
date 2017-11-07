b8d3a0733586e82ee20d0d6208636be12643728d
Carlos Santana <csantana@us.ibm.com>
2017-11-06 22:32:32 -0500

    fix test reference using wskRest now

--- tests/src/test/scala/system/basic/WskUnicodeIBMNodeTests.scala
+++ tests/src/test/scala/system/basic/WskUnicodeIBMNodeTests.scala
@@ -18,11 +18,8 @@ package system.basic
 import org.junit.runner.RunWith
 import org.scalatest.junit.JUnitRunner
 
-import common.JsHelpers
-import common.WskTestHelpers
-
 @RunWith(classOf[JUnitRunner])
-class WskUnicodeIBMNodeTests extends WskUnicodeTests with WskTestHelpers with JsHelpers {
+class WskUnicodeIBMNodeTests extends WskRestUnicodeNodeTests {
 
   override lazy val actionKind = "nodejs-ibm:8"
   override lazy val actionSource = "unicode.js"
