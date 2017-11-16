73baa53a74ac93fe595aa75031ab001d2009b535
Carlos Santana <csantana@us.ibm.com>
2017-11-15 21:51:36 -0500

    rename docker image to action-nodejs-v8

--- tests/src/test/scala/actionContainers/IBMNodeJsActionContainerTests.scala
+++ tests/src/test/scala/actionContainers/IBMNodeJsActionContainerTests.scala
@@ -21,6 +21,6 @@ import org.scalatest.junit.JUnitRunner
 @RunWith(classOf[JUnitRunner])
 class IBMNodeJsActionContainerTests extends NodeJsActionContainerTests {
 
-  override lazy val nodejsContainerImageName = "action-nodejs-ibm-v8"
+  override lazy val nodejsContainerImageName = "action-nodejs-v8"
 
 }
