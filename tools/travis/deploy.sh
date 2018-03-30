8c0a368f1f78a8d853ad71c8616da4b47700e730
Carlos Santana <csantanapr@apache.org>
2018-03-30 18:45:38 -0400

    fix travis missing vcap file

--- tools/travis/deploy.sh
+++ tools/travis/deploy.sh
@@ -1,21 +1,4 @@
 #!/bin/bash
-#
-# Licensed to the Apache Software Foundation (ASF) under one or more
-# contributor license agreements.  See the NOTICE file distributed with
-# this work for additional information regarding copyright ownership.
-# The ASF licenses this file to You under the Apache License, Version 2.0
-# (the "License"); you may not use this file except in compliance with
-# the License.  You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-
 set -ex
 
 # Build script for Travis-CI.
@@ -41,7 +24,13 @@ $ANSIBLE_CMD openwhisk.yml -e cli_installation_mode=remote
 docker images
 docker ps
 
-cat $WHISKDIR/whisk.properties
+#update whisk.properties to add tests/credentials.json file to vcap.services.file, which is needed in tests
+VCAP_SERVICES_FILE="$(readlink -f ${ROOTDIR}/tests/credentials.json)"
+WHISKPROPS_FILE="$WHISKDIR/whisk.properties"
+sed -i 's:^[ \t]*vcap.services.file[ \t]*=\([ \t]*.*\)$:vcap.services.file='$VCAP_SERVICES_FILE':'  $WHISKPROPS_FILE
+
+cat $WHISKPROPS_FILE
+
 curl -s -k https://172.17.0.1 | jq .
 curl -s -k https://172.17.0.1/api/v1 | jq .
 
@@ -50,7 +39,7 @@ WHISK_APIHOST="172.17.0.1"
 WHISK_AUTH=`cat ${WHISKDIR}/ansible/files/auth.guest`
 WHISK_CLI="${WHISKDIR}/bin/wsk -i"
 
-${WHISK_CLI} property set --apihost ${WHISK_APIHOST} --auth ${WHISK_AUTH} 
+${WHISK_CLI} property set --apihost ${WHISK_APIHOST} --auth ${WHISK_AUTH}
 ${WHISK_CLI} property get
 
 
