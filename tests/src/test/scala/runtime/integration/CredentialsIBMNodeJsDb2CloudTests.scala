051b093dd3483be5a7ccbcdb3e0bc0d2b1b6cad6
Carlos Santana <csantanapr@apache.org>
2018-11-09 13:14:21 -0500

    move integration tests under runtime package

--- tests/src/test/scala/runtime/integration/CredentialsIBMNodeJsDb2CloudTests.scala
+++ tests/src/test/scala/runtime/integration/CredentialsIBMNodeJsDb2CloudTests.scala
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package integration
+package runtime.integration
 
 import common._
 import common.rest.WskRestOperations
