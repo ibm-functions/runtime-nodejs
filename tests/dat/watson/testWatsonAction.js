eab96a6c78d0f2a73417a571313cf422e8f5bda0
Carlos Santana <csantanapr@apache.org>
2019-02-19 22:43:52 -0500

    bump watson sdk to v4.0.0-rc2 for nodejs10

--- tests/dat/watson/testWatsonAction.js
+++ tests/dat/watson/testWatsonAction.js
@@ -1,10 +1,5 @@
-
-
-const nodeRuntime = process.version.startsWith('v8.') ? 'nodejs8' : 'nodejs10'
-const isNodeJS8 = nodeRuntime === 'nodejs8' ? true : false
-
 // This will error out if watson not available
-var DiscoveryV1 = isNodeJS8 ? require('watson-developer-cloud/discovery/v1') : require('watson-developer-cloud/discovery/v1-generated')
+var DiscoveryV1 = require('watson-developer-cloud/discovery/v1')
 
 function main(args){
   return {}
