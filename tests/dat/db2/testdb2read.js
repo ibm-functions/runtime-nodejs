a83687f4b3f2de72e1973c9d3c81918f5fa288ec
Falk Zoll <FALKZOLL@de.ibm.com>
2020-01-10 19:20:25 +0100

    Derive db2 schema name from ssldsn UID value.   - This is required for to the db2 free plan we now use.

--- tests/dat/db2/testdb2read.js
+++ tests/dat/db2/testdb2read.js
@@ -8,11 +8,25 @@ function main({
   }
 }) {
   let result, rows;
+
+  // For the free db2 database plan, the schema name
+  // needs to be the same as the uid of the ssldsn.
+  // Therefore we split the ssldsn at the ';' and read the
+  // individual key value pairs into a map.
+  // From the map we then can easily access the UID.
+  var ssldsnMap = {};
+  ssldsn.split(';').forEach(function(x){
+    var arr = x.split('=');
+    arr[1] && (ssldsnMap[arr[0]] = arr[1]);
+  });
+
+  console.log("UID="+ssldsnMap["UID"]);
+
   try {
     if(!connection){
       connection = ibmdb.openSync(ssldsn);
     }
-    rows = connection.querySync(`SELECT HISP_DESC FROM SAMPLES.HISPANIC_ORIGIN WHERE HISP_CODE='03'`);
+    rows = connection.querySync(`SELECT HISP_DESC FROM ${ssldsnMap["UID"]}.HISPANIC_ORIGIN WHERE HISP_CODE='03'`);
     return rows[0];
   } catch (err) {
     console.error('problems reading from db',err);
