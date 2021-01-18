ba9bb97e659735c035120523a86cf380b4282853
falkzoll <falkzoll@de.ibm.com>
2021-01-18 10:49:19 +0100

    Login to hub.docker.com to get user specific pull rate. (#218)

--- tools/travis/build.sh
+++ tools/travis/build.sh
@@ -28,6 +28,12 @@ export OPENWHISK_HOME=$WHISKDIR
 
 IMAGE_PREFIX="testing"
 
+# Login to hub.docker.com to get user specific pull rate.
+if [ ! -z "${DOCKER_USER}" ] && [ ! -z "${DOCKER_PASSWORD}" ]; then
+  echo "Run docker login..."
+  echo ${DOCKER_PASSWORD} | docker login -u "${DOCKER_USER}" --password-stdin
+fi
+
 # Build OpenWhisk
 cd $WHISKDIR
 
