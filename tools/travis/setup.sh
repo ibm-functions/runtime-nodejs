bd4e63d3fa727cab8d6f4168ce8ec6bbe457dd81
falkzoll <falkzoll@de.ibm.com>
2021-06-14 09:03:59 +0200

    Use fixed apache/openwhisk commit for tests. (#228)
    - Use a fixed apache/openwhisk commit to run the tests, to explicitly control when changes are consumed.

--- tools/travis/setup.sh
+++ tools/travis/setup.sh
@@ -26,7 +26,10 @@ HOMEDIR="$SCRIPTDIR/../../../"
 
 # OpenWhisk stuff
 cd $HOMEDIR
-git clone --depth=1 https://github.com/apache/incubator-openwhisk.git openwhisk
+#git clone --depth=1 https://github.com/apache/incubator-openwhisk.git openwhisk
+git clone https://github.com/apache/incubator-openwhisk.git openwhisk
 cd openwhisk
+# Use a fixed commit to run the tests, to explicitly control when changes are consumed.
+git checkout ecb2a980659f28d0adbd9ef837afaf4cb2b695bf
 ./tools/travis/setup.sh
 
