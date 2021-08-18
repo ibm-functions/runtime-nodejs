89e60fa92e36004af89ed6f5b21f4166a094cee3
Falk Zoll <FALKZOLL@de.ibm.com>
2021-08-18 15:45:36 +0200

    Adapt to new apache/openwhisk commit. - Due to the removal of version 12.6.0 of the azure-storage-blob module on maven central openwhisk was updated to 12.7.0. Because of this we also needed to update to this new commit of openwhisk.

--- tools/travis/setup.sh
+++ tools/travis/setup.sh
@@ -26,10 +26,13 @@ HOMEDIR="$SCRIPTDIR/../../../"
 
 # OpenWhisk stuff
 cd $HOMEDIR
-#git clone --depth=1 https://github.com/apache/incubator-openwhisk.git openwhisk
+
+# Clone and setup openwhisk to have a local test environment.
 git clone https://github.com/apache/incubator-openwhisk.git openwhisk
 cd openwhisk
 # Use a fixed commit to run the tests, to explicitly control when changes are consumed.
-git checkout ecb2a980659f28d0adbd9ef837afaf4cb2b695bf
+# Commit: minor version bump of azure-storage-blob to fix builds (#5150)
+git checkout 3e6138d088fbd502a69c31314ad7c0089c5f5283
+
 ./tools/travis/setup.sh
 
