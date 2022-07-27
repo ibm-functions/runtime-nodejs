988c727bc76cdbe6a7dc344fb63f349b40300749
Luke-Roy-IBM <83647952+Luke-Roy-IBM@users.noreply.github.com>
2022-07-27 15:01:19 +0200

    Update openwhisk to tag 8c140e0d5baec8e760832ca98d8bcc66664a0146 (#245)
    * Update components for build and test to use the ibm-functions/openwhisk repo and to use the ibmfunctions/controller and ibmfunctions/invoker images.
    * Lower akka version to match version of ibm-functions/openwhisk repo.

--- tools/travis/setup.sh
+++ tools/travis/setup.sh
@@ -28,11 +28,11 @@ HOMEDIR="$SCRIPTDIR/../../../"
 cd $HOMEDIR
 
 # Clone and setup openwhisk to have a local test environment.
-git clone https://github.com/apache/incubator-openwhisk.git openwhisk
+git clone  https://github.com/ibm-functions/openwhisk.git openwhisk
 cd openwhisk
 # Use a fixed commit to run the tests, to explicitly control when changes are consumed.
-# Commit: minor version bump of azure-storage-blob to fix builds (#5150)
-git checkout 3e6138d088fbd502a69c31314ad7c0089c5f5283
+# Commit: Dedicated Invokers (#5292) 
+git checkout 7ae02b8ede4f4b4068b3b95dbc3f02f902d936c9
 
 ./tools/travis/setup.sh
 
